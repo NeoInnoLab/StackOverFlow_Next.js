e55e534d5a8ee703352f6d407206809e0e96d2cb
S.W <127256370+NeoInnoLab@users.noreply.github.com>
2024-03-01 07:18:21 -0500

    add clerk secret

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ["/"] });
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "api/chatgpt"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
