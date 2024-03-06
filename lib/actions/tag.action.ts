a32ef7b96ac1a182ef3461de29fddd09e2746ddc
S.W <127256370+NeoInnoLab@users.noreply.github.com>
2024-03-06 16:09:38 -0500

    set Tags Page, TagCard, getAllTags()

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import User from "@/database/user.model";
+import Tag from "@/database/tag.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -21,6 +22,21 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "1", name: "tag1" },
       { _id: "2", name: "tag2" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
